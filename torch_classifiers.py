a4af1e0984cd5a360d0c3d54dd13a7f8d8d4c062
Zhou Yang <55669475+yangzhou6666@users.noreply.github.com>
2022-03-25 11:51:15 +0800

    refactor(part2): run random forest

--- torch_classifiers.py
+++ torch_classifiers.py
@@ -129,18 +129,15 @@ def run_random_forest(train_path, test_path, vbose=False):
     X_train, Y_train, X_test, Y_test = load_data(train_path, test_path)
     X_train, X_test = torch.Tensor(X_train),torch.Tensor(X_test)
     y_train, y_test = torch.Tensor(Y_train),torch.Tensor(Y_test)
-    my_model = TorchRandomForestClassifier(nb_trees=3, nb_samples=X_train.shape[0], max_depth=2, bootstrap=True)
-    print("Model Created.")
-    print("Training...")
+    my_model = TorchRandomForestClassifier(nb_trees=10, nb_samples=X_train.shape[0], max_depth=3, bootstrap=True)
     my_model.fit(X_train, y_train)
-    print("Training Complete.")
     
     # Prediction function
     predicted_results = []
     for input in X_test:
         predicted_results.append(my_model.predict(input))
 
-    accuracy = np.sum(predicted_results == y_test.detach().numpy()) / len(predicted_results)
+    accuracy = 100 * np.sum(predicted_results == y_test.detach().numpy()) / len(predicted_results)
     
     TP = np.sum(predicted_results * y_test.detach().numpy())
     FP = np.sum(predicted_results * (1-y_test.detach().numpy()))
@@ -156,22 +153,7 @@ def run_random_forest(train_path, test_path, vbose=False):
     return accuracy, precision, recall, f_1
 
 
-if __name__=="__main__":
-    split_type = ['k_folder', 's_k_folder']
-    root_data = './part_2'
-    train_path = './part_2/k_folder/train_1.txt'
-    test_path = './part_2/k_folder/test_1.txt'
-    k = 10
-    # train_path = './part_1/train.txt'
-    # test_path = './part_1/test.txt'
-    dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-
-    run_random_forest(train_path, test_path, vbose=True)
-
-    exit()
-    random.seed(0)
-    np.random.seed(0)
-    torch.manual_seed(0)
+def main_lr(split_type, root_data, k):
     for type in split_type:
         accs = []
         precisions = []
@@ -193,6 +175,49 @@ if __name__=="__main__":
         print('|' + 'avg' + '|' + str(round(sum(accs) / k, 2)) + '%|' + str(round(sum(precisions) / k, 2)) + '%|' + str(round(sum(recalls) / k, 2)) + '%|' + str(round(sum(f1s) / k, 2)) + '%|')
         
         print("\n\n")
+
+
+def main_rf(split_type, root_data, k):
+    '''main function for random forest'''
+    for type in split_type:
+        accs = []
+        precisions = []
+        recalls = []
+        f1s = []
+        print(f"Results for {type} split:")
+        for i in range(k):
+            train_path = os.path.join(root_data, type, f'train_{i}.txt')
+            test_path = os.path.join(root_data, type, f'test_{i}.txt')
+            accuracy_test, precision, recall, f_1 = run_random_forest(train_path, test_path)
+            accs.append(accuracy_test)
+            precisions.append(precision)
+            recalls.append(recall)
+            f1s.append(f_1)
+            print('|' + str(i) + '|' + str(accs[i]) + '%|' + str(precisions[i]) + '%|' + str(recalls[i]) + '%|' + str(f1s[i]) + '%|')
+        
+    
+        
+        print('|' + 'avg' + '|' + str(round(sum(accs) / k, 2)) + '%|' + str(round(sum(precisions) / k, 2)) + '%|' + str(round(sum(recalls) / k, 2)) + '%|' + str(round(sum(f1s) / k, 2)) + '%|')
+        
+        print("\n\n")
+
+
+if __name__=="__main__":
+    split_type = ['k_folder', 's_k_folder']
+    root_data = './part_2'
+    train_path = './part_2/k_folder/train_1.txt'
+    test_path = './part_2/k_folder/test_1.txt'
+    k = 10
+    # train_path = './part_1/train.txt'
+    # test_path = './part_1/test.txt'
+    dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    random.seed(0)
+    np.random.seed(0)
+    torch.manual_seed(0)
+
+    main_rf(split_type, root_data, k)
+
             
 
     
